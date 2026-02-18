190c7fadafa81ec5c639ed60af9973564eedfe28
Shakti Thakur <imshakti01234@gmail.com>
2026-02-18 13:00:23 +0545

    finish project

---
+++ scrape.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+
+#url of the website to scrap
+url="http://books.toscrape.com/"
+
+
+def scrape_books(url):
+    response=requests.get(url)
+    if response.status_code!=200:
+        return
+    
+
+    #set encoding expicity to handle special character correctly
+    response.encoding=response.apparent_encoding
+
+    soup=BeautifulSoup(response.text,"html.parser")
+    books=soup.find_all("article",class_="product_pod")
+    list_book=[]
+    for book in books:
+        title=book.h3.a['title']
+       
+        price_text=book.find('p',class_='price_color').text
+        currency=price_text[0]
+        price=float(price_text[1:])
+        print(title,price,currency)
+        book_info = {
+        "title": title,
+        "price": price,
+        "currency": currency
+        }
+
+        list_book.append(book_info)
+
+
+    #import json
+        
+    #with open("books.json", "w", encoding="utf-8") as file:
+       # json.dump(list_book, file, indent=4, ensure_ascii=False)
+
+    #pythonprint(books.json,type(books.json))    
+
+
+    import csv
+    with open("books.csv","w",encoding="utf-8") as file:
+        writer=csv.DictWriter(file,fieldnames=["title","price","currency"])
+        writer.writeheader()
+        writer.writerows(list_book)
+
+
+
+
+scrape_books(url)
